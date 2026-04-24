ae81347e9d692774770102cb8378a50eff4769f9
Manikam-314 <manikavasakamemail@gmail.com>
2026-04-24 11:18:39 +0530

    Add: buy-and-sell-crypto - submission-4

---
+++ Data Structures & Algorithms/buy-and-sell-crypto/submission-4.java
@@ -0,0 +1,19 @@
+class Solution {
+    public int maxProfit(int[] prices) {
+
+        int minPrice = Integer.MAX_VALUE;
+        int maxProfit = 0;
+
+        for (int price : prices) {
+
+            if (price < minPrice) {
+                minPrice = price; // buy here
+            } else {
+                int profit = price - minPrice; // sell here
+                maxProfit = Math.max(maxProfit, profit);
+            }
+        }
+
+        return maxProfit;
+    }
+}
