9980d1602417eece03ce090c901cbe9bdec10b98
Manikam-314 <manikavasakamemail@gmail.com>
2026-04-29 14:11:59 +0530

    Add: find-median-in-a-data-stream - submission-1

---
+++ Data Structures & Algorithms/find-median-in-a-data-stream/submission-1.java
@@ -0,0 +1,25 @@
+class MedianFinder {
+        PriorityQueue<Integer> maxHeap;
+        PriorityQueue<Integer> minHeap;
+    public MedianFinder() {
+        maxHeap = new PriorityQueue<>(Collections.reverseOrder());
+        minHeap = new PriorityQueue<>();
+    }
+    
+    public void addNum(int num) {
+        maxHeap.offer(num);
+
+        minHeap.offer(maxHeap.poll());
+
+        if(maxHeap.size()<minHeap.size()){
+            maxHeap.offer(minHeap.poll());
+        }
+    }
+    
+    public double findMedian() {
+        if(maxHeap.size()==minHeap.size()){
+            return (maxHeap.peek() + minHeap.peek()) / 2.0;
+        }
+        return maxHeap.peek();
+    }
+}
